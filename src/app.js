1118128bc35a78fa01b40f5902fa824ddab3bb0c
Adão Lima <adaoolima@gmail.com>
2020-04-10 19:31:12 -0300

    Added middlware to validadte ID and some tips from live

--- src/app.js
+++ src/app.js
@@ -10,12 +10,19 @@ app.use(cors());
 
 const repositories = [];
 
+function checkID(request, response, next) {
+  const { id } = request.params;
+  if(repositories.filter(item => item.id === id).length === 0) {
+    return response.status(400).json({ "error": "ID não encontrado"});
+  }
+  next();
+}
+
 app.get("/repositories", (request, response) => {
   return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  
   const {
     title, 
     url,
@@ -35,38 +42,31 @@ app.post("/repositories", (request, response) => {
 
 });
 
-app.put("/repositories/:id", (request, response) => {
+app.put("/repositories/:id",checkID, (request, response) => {
   const {id} = request.params;
   const { title, url, techs } = request.body;
-  const likes = repositories.find(item => item.id === id ? item.likes : 0);
 
-  if(repositories.filter(item => item.id === id).length === 0) {
-    return response.status(400).json({ "error": "ID não encontrado"});
-  }
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
 
+  let repository;
   repositories.map(item => {
     if(item.id === id) {
-      title ? item.title = title : '';
-      url ? item.url = url : '';
-      techs ? item.techs = techs : '';
+      repository = {
+        id,
+        title: title || item.title,
+        url: url || item.url,
+        techs: techs || item.techs,
+        likes: repositories[findRepositoryIndex].likes
+      }
+      item = repository;
     }
   });
-  return response.status(200).json({
-    id,
-    title,
-    url,
-    techs
-  });
+  return response.status(200).json(repository);
 
 });
 
-app.delete("/repositories/:id", (request, response) => {
+app.delete("/repositories/:id", checkID, (request, response) => {
   const { id } = request.params;
-  
-  if(repositories.filter(item => item.id === id).length === 0) {
-    return response.status(400).json({ "error": "ID não encontrado"});
-  }
-  if(repositories.length !== 0 ){
     repositories.map((item, index) => {
       if(item.id === id) {
         console.log(item.id , index);
@@ -74,30 +74,17 @@ app.delete("/repositories/:id", (request, response) => {
         return response.status(204).send('Content deleted');
       }
     });
-  } else {
-    return response.status(400).json({ "error": "ID não encontrado"});
-  }
-
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  const {id} = request.params;
-  const { method } = request;
-  let repositoryLiked;
-  if(method !== 'POST') return response.status(300).json({ "error": "ROTA ERRA"});
-  if(repositories.filter(item => item.id === id).length === 0) {
-    return response.status(400).json({ "error": "ID não encontrado"});
-  } else {
-    repositories.map(item => {
-    if(item.id === id) {
-      item.likes += 1;
-    }
-    repositoryLiked = item;
-  });
-  return response.json(repositoryLiked);
-  }
+app.post("/repositories/:id/like",checkID, (request, response) => {
+  const { id } = request.params;
 
-  
+  const findRepositoryIndex = repositories.findIndex(repo => repo.id === id);
+
+  repositories[findRepositoryIndex].likes += 1;
+
+  return response.json(repositories[findRepositoryIndex]);
+
 });
 
 module.exports = app;
